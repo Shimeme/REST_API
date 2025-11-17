c2abf90041a5ac43ef4205ea6b5e8a5499a7b7f7
Shimeme <georgegolubevv@gmail.com>
2025-11-17 09:28:45 +0900

    adding files

---
+++ cmd/api/events.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"REST_API/cmd/internal/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (app *application) createEvent(c *gin.Context) {
+	var event database.Event
+
+	if err := c.ShouldBindJSON(&event); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := app.models.Events.insert(&event)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create event"})
+		return
+	}
+	c.JSON(http.StatusCreated, event)
+}
+
+func (app *application) getEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event id"})
+		return
+	}
+
+	event, err := app.models.Events.Get(id)
+
+	if event == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "event not found"})
+		return
+	}
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retreave event"})
+		return
+	}
+
+	c.JSON(http.StatusOK, event)
+}
